3c9f33bd391c8a8d9f44520b4f3867cf19c80771
cbrasser <brasser13@gmail.com>
2016-11-14 21:23:34 +0100

    Add files via upload

---
+++ Climattention/test-classes/FieldVerifierTest.java
@@ -0,0 +1,14 @@
+package com.climattention.shared;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FieldVerifierTest {
+
+	@Test
+	public void testIsValidName() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Climattention/test-classes/GreetingServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.climattention.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GreetingServiceImplTest {
+
+	@Test
+	public void testGreetServer() {
+		fail("Not yet implemented");
+	}
+
+}
