08e561e23d1642a7be134697de8101205b4560d3
jfuhrer <jara.fuhrer@hotmail.com>
2016-11-25 13:30:39 +0100

    template for JUnit tests for climattention
    does anyone know how to do it?!?

---
+++ src/com/climattention/client/ClimattentionTest.java
@@ -0,0 +1,67 @@
+package com.climattention.client;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class ClimattentionTest {
+
+	@Test
+	public void testOnModuleLoad() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	private void testCreateUserInterface() {
+			fail("Not yet implemented");
+	}
+	
+	@Test
+	private void testCreateMap(VerticalPanel Vertmap){
+	
+	}
+	
+	@Test
+	private void testCreateTable(VerticalPanel tableViewLayout){
+		
+	}
+	
+	@Test
+	private void testReloadTable() {
+		
+	}
+	
+	@Test
+	private void testAddUncertaintyFromSorter(){
+		
+	}
+	
+	@Test
+	private void testAddUncertaintyToSorter(){
+		
+	}
+	
+	@Test
+	private void testAddYearFromSorter(){
+		
+	}
+	
+	@Test
+	private void testAddYearToSorter(){
+		
+	}
+	
+	@Test
+	private void testAddCityNameSorter(){
+		
+	}
+	
+	@Test
+	private void testAddCountryNameSorter(){
+		
+	}
+
+}
