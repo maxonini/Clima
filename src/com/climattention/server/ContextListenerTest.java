5fdf344e188405c3ee618bb86763abeaf0e0886a
valerieerb <valerie.erb@uzh.ch>
2016-11-28 17:32:55 +0100

    ContextListenerTest
    Could not implement contextinitializedtest, Claudio?

--- src/com/climattention/server/ContextListenerTest.java
+++ src/com/climattention/server/ContextListenerTest.java
@@ -3,12 +3,23 @@ package com.climattention.server;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import com.climattention.shared.Datapoint;
 
 public class ContextListenerTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testContextInitialized() {
+		// claudio kannst du das hier anschauen?
+		ContextListener listen = new ContextListener();
+		ServletContextEvent event = new ServletContextEvent(null);
+		// nulll als parameter is nicht optimal, aber was für parameter sollten das sein?
+		
+		listen.contextInitialized(event);
+		
+		
 	}
 
 }
