5ca0095ab26e0ba3dd242fc24d3ed798d9443f4f
jfuhrer <jara.fuhrer@hotmail.com>
2016-12-01 15:15:29 +0100

    last two arguments to string (damit kein error mehr kommt)

--- src/com/climattention/client/MyTableTest.java
+++ src/com/climattention/client/MyTableTest.java
@@ -13,9 +13,9 @@ public class MyTableTest {
 		
 		MyTable table = new MyTable();
 		
-		Datapoint p1 = new Datapoint("1.1.95", 37.2f ,0.03f, "Paris", "France",1000f,2008f );
-		Datapoint p2 = new Datapoint("1.8.95", 36.2f ,0.43f, "New York", "USA",1056f,2208f );
-		Datapoint p3 = new Datapoint("1.4.95", 33.2f ,0.3f, "London", "England",1600f,2041f );
+		Datapoint p1 = new Datapoint("1.1.95", 37.2f ,0.03f, "Paris", "France","1000f","2008f" );
+		Datapoint p2 = new Datapoint("1.8.95", 36.2f ,0.43f, "New York", "USA","1056f","2208f" );
+		Datapoint p3 = new Datapoint("1.4.95", 33.2f ,0.3f, "London", "England","1600f","2041f" );
 		
 		Datapoint[] places = new Datapoint[3];
 		places[0] = p1;
