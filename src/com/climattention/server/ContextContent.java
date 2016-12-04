d794da2c0eff608fdf29a49a03fdc4d1c4fe3030
cbrasser <brasser13@gmail.com>
2016-12-04 17:37:55 +0100

    added second context property

--- src/com/climattention/server/ContextContent.java
+++ src/com/climattention/server/ContextContent.java
@@ -2,5 +2,6 @@ package com.climattention.server;
 
 public class ContextContent {
 	public static final String CLIMATE_DATA = "climateData";
+	public static final String AVERAGE_PER_YEAR = "AVERAGE_PER_YEAR";
 
 }
