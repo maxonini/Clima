09076b3511e026e9caefabfcbd637ee08cd6d629
cbrasser <brasser13@gmail.com>
2016-12-05 12:30:26 +0100

    fix

--- src/com/climattention/client/ClimaService.java
+++ src/com/climattention/client/ClimaService.java
@@ -4,7 +4,7 @@ import com.climattention.shared.AverageData;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
-@RemoteServiceRelativePath("Clima")
+@RemoteServiceRelativePath("averageYear")
 public interface ClimaService extends RemoteService{
 	
 	AverageData[] getAverageForYear(int year);
